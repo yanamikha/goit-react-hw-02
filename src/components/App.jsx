e99e6758f81c411b5084f55f0a32370167adceb6
yanamikha <yanamikha@gmail.com>
2026-02-27 16:21:28 +0100

    ts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -32,7 +32,7 @@ function App() {
   return (
     <>
       <embed
-        src='blob:https://ln9-ptl01-dr.isd.dp.ua:8222/461cc0aa-4c04-47d0-9771-d8ce2041da3d'
+        src='https://ln9-ptl01-dr.isd.dp.ua:8222/461cc0aa-4c04-47d0-9771-d8ce2041da3d'
         type="application/pdf"
         className="h-[70vh] w-full rounded-b-lg"
       />
